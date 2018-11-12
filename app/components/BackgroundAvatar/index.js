fa9b86786ce836698defc984b8e22df8626f0e26
Kevin Gonzalez <typekev@gmail.com>
2018-11-13 00:11:35 +0100

    new avatar 2018

--- app/components/BackgroundAvatar/index.js
+++ app/components/BackgroundAvatar/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import BackgroundAvatarContainer from './BackgroundAvatarContainer';
 import typekevAvatar from 'images/typekev-avatar-wpap.svg';
+
+import BackgroundAvatarContainer from './BackgroundAvatarContainer';
 
 export default function BackgroundAvatar(props) {
   return <BackgroundAvatarContainer src={typekevAvatar} {...props} />;
