978760c53be392b234a3c3563d72029b3a332ce3
Kevin Gonzalez <typekev@gmail.com>
2019-08-25 23:19:36 +0200

    Update drawer interactions

--- src/components/Drawer/tests/index.test.js
+++ src/components/Drawer/tests/index.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Drawer, { Spacer, Button } from 'components/Drawer';
 
 describe('Drawer component', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Drawer open toggleDrawer={() => {}} />, div);
+    ReactDOM.render(
+      <Router>
+        <Drawer open toggleDrawer={() => {}} />
+      </Router>,
+      div,
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 
