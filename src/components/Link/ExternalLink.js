a8d96814f38a54d979df9a38cb4554d44922b8b2
Kevin Gonzalez <typekev@gmail.com>
2019-08-26 13:14:45 +0200

    Add tests to Link component

--- src/components/Link/ExternalLink.js
+++ src/components/Link/ExternalLink.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 /* eslint-disable react/jsx-props-no-spreading */
 
-function ExternalLink({ children, href, ...rest }) {
+function ExternalLink({ children, href, innerRef, ...rest }) {
   return (
     <a href={href} target="_blank" rel="noopener noreferrer" {...rest}>
       {children}
@@ -14,6 +14,11 @@ function ExternalLink({ children, href, ...rest }) {
 ExternalLink.propTypes = {
   children: PropTypes.node.isRequired,
   href: PropTypes.string.isRequired,
+  innerRef: PropTypes.func,
+};
+
+ExternalLink.defaultProps = {
+  innerRef: undefined,
 };
 
 export default React.forwardRef((props, ref) => <ExternalLink innerRef={ref} {...props} />);
