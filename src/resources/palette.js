158a1873f80241948bb7355f05b0c41b686d7663
Kevin Gonzalez <typekev@gmail.com>
2019-08-29 15:35:42 +0200

    Update theme colors to use HEX

--- src/resources/palette.js
+++ src/resources/palette.js
@@ -1,9 +1,13 @@
 const palette = {
   primary: {
     main: '#AFC8E0',
+    dark: '#7A8C9C',
+    light: '#BFD3E6',
   },
   secondary: {
     main: '#F9F3AA',
+    dark: '#AEAA76',
+    light: '#FAF5BB',
   },
   error: {
     main: '#D33332',
