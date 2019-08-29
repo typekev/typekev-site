336e9e971d22cafe1070fc06cac1266440aa4efc
Kevin Gonzalez <typekev@gmail.com>
2019-08-29 15:38:03 +0200

    Add Section component

--- src/templates/Page/index.js
+++ src/templates/Page/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Section from 'templates/Section';
 import Sun from 'templates/Page/Sun';
 import Ray from 'templates/Page/Ray';
+import Stars from 'templates/Page/Stars';
 
 export default function Page({ children }) {
   return (
     <>
-      {children}
+      <Stars />
       <Ray delay={0} />
       <Ray delay={0.125} />
       <Ray delay={0.75} />
       <Ray delay={1.5} />
-      <Ray delay={3} />
+      <Ray delay={2.75} />
       <Sun />
+      <Section>{children}</Section>
     </>
   );
 }
---
+++ src/templates/Section/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Section = styled.section`
+  flex: 1 1 100%;
+  max-width: 100%;
+  overflow: hidden scroll;
+`;
+
+export default Section;
