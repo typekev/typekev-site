cacd5762037fbf86007d605180bc4df7b1ff0955
Kevin Gonzalez <typekev@gmail.com>
2019-08-29 15:45:22 +0200

    Add canvas mock to stop tests from throwing warnings

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import 'jest-canvas-mock';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 /* eslint-enable import/no-extraneous-dependencies */
