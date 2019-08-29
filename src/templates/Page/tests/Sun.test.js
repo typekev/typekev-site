7a7a30c88ad1a6b458c314649afb6d96d556f0ec
Kevin Gonzalez <typekev@gmail.com>
2019-08-30 01:08:36 +0200

    Fix test for Sun component

--- src/templates/Page/tests/Sun.test.js
+++ src/templates/Page/tests/Sun.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Sun from 'templates/Page/Sun';
 
 describe('Sun component', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Sun />, div);
+    ReactDOM.render(
+      <Router>
+        <Sun />
+      </Router>,
+      div,
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 });
