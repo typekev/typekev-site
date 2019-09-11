1192b3e774dbfcb67dca64db1a6eeb7f9c996694
Kevin Gonzalez <typekev@gmail.com>
2019-09-11 16:00:43 +0200

    Use react-mk for keyboard effect

--- src/routes/Explore/index.js
+++ src/routes/Explore/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Keyboard, { Cursor } from 'react-mk';
 import TextField from '@material-ui/core/TextField';
 import Grow from '@material-ui/core/Grow';
 import Content from 'templates/Content';
@@ -8,9 +9,19 @@ import Title from 'components/Title';
 export default function Explore() {
   return (
     <Transition in component={Grow}>
-      <Content align="center">
+      <Content align="center" maxWidth="md">
         <Title variant="h6" align="center">
-          Explore
+          <Keyboard>
+            {({ type }) =>
+              type(
+                400,
+                'Welcome, visitor',
+                "I'm Kevin's autonomous assistant",
+                'What can I help you with?',
+              )
+            }
+          </Keyboard>
+          <Cursor />
         </Title>
         <TextField
           id="chatbot-input"
