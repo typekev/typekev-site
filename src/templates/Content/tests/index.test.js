4c0b3ca363e64da32450194a4eff32fc8383d78c
Kevin Gonzalez <typekev@gmail.com>
2019-09-18 19:14:31 +0200

    Fix children required error on Content test

--- src/templates/Content/tests/index.test.js
+++ src/templates/Content/tests/index.test.js
@@ -5,13 +5,13 @@ import Content from 'templates/Content';
 describe('Content component', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Content />, div);
+    ReactDOM.render(<Content>Test</Content>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
   it('renders with center align without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Content align="center" />, div);
+    ReactDOM.render(<Content align="center">Test</Content>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
