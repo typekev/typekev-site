091a81e287f128ba619d686d23cf1ae1e27f9c59
Kevin Gonzalez <typekev@gmail.com>
2019-09-18 19:21:30 +0200

    Make logError test smarter

--- src/utils/tests/request.test.js
+++ src/utils/tests/request.test.js
@@ -1,8 +1,15 @@
 import { logError } from 'utils/request';
 
+global.console = {
+  log: jest.fn(),
+  info: jest.fn(),
+  error: jest.fn(),
+};
+
 describe('request function', () => {
   it('returns undefined on catch', () => {
-    const error = new Error('Error Message');
-    expect(logError(error)).toBe(undefined);
+    const errorMessage = 'Error Message';
+    logError(errorMessage);
+    expect(global.console.error).toHaveBeenCalledWith('request failed', errorMessage);
   });
 });
