780080f0fc3b7754ba0d914c30444b683cdd3288
Kevin Gonzalez <typekev@gmail.com>
2019-09-19 22:19:28 +0200

    Add tests for useChat hook

--- src/hooks/useChat.js
+++ src/hooks/useChat.js
@@ -1,18 +1,21 @@
 import { useState } from 'react';
 import { getDirectLineConversation, sendDirectLineMessage } from 'routes/Explore/api';
 
-const getConversation = async setConversation => {
+export const initialState = {};
+
+export const getConversation = async setConversation => {
   try {
     const conversation = await getDirectLineConversation();
     setConversation(conversation);
   } catch (err) {
-    setConversation(null);
+    setConversation(initialState);
   }
 };
 
 export default function useChat() {
-  const [{ conversationId, token, streamUrl }, setConversation] = useState({});
+  const [{ conversationId, token, streamUrl }, setConversation] = useState(initialState);
   const startChat = () => getConversation(setConversation);
-  const sendMessage = text => text && sendDirectLineMessage({ conversationId, token, text });
+  const sendMessage = text =>
+    typeof text === 'string' && sendDirectLineMessage({ conversationId, token, text });
   return [{ conversationId, token, streamUrl }, startChat, sendMessage];
 }
