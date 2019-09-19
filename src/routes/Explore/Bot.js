ec26dbe068a25734adf5b6277d6e7855b79978c0
Kevin Gonzalez <typekev@gmail.com>
2019-09-19 21:48:43 +0200

    Add tests for Bot component

--- src/routes/Explore/Bot.js
+++ src/routes/Explore/Bot.js
@@ -2,6 +2,18 @@ import { useEffect, useRef } from 'react';
 import useSocket from 'hooks/useSockets';
 import debounce from 'lodash.debounce';
 
+export const setInitialSocket = (streamUrl, socket, setSocket) =>
+  streamUrl && !socket && setSocket(streamUrl);
+
+export const handleSetMessage = (setMessages, messagesRef, clearMessages) => () => {
+  setMessages(messagesRef.current);
+  clearMessages();
+};
+
+export const debounceMessages = (messages, setMessages, messagesRef, clearMessages) =>
+  messages.length > 0 &&
+  debounce(handleSetMessage(setMessages, messagesRef, clearMessages), 3000)();
+
 export default function Bot({ startChat, streamUrl, setMessages }) {
   const [socket, setSocket, messages, clearMessages] = useSocket();
   const messagesRef = useRef(messages);
@@ -13,19 +25,12 @@ export default function Bot({ startChat, streamUrl, setMessages }) {
   }, []);
 
   useEffect(() => {
-    if (streamUrl && !socket) {
-      setSocket(streamUrl);
-    }
+    setInitialSocket(streamUrl, socket, setSocket);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [streamUrl]);
 
   useEffect(() => {
-    if (messages.length > 0) {
-      debounce(() => {
-        setMessages(messagesRef.current);
-        clearMessages();
-      }, 3000)();
-    }
+    debounceMessages(messages, setMessages, messagesRef, clearMessages);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [messages.length]);
 
