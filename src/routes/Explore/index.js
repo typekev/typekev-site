ae2cbb7e6caa358ed1ee99f2dbf2ff697e48c51e
Kevin Gonzalez <typekev@gmail.com>
2019-10-19 23:26:28 +0200

    Clean up Explore route

--- src/routes/Explore/index.js
+++ src/routes/Explore/index.js
@@ -1,24 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Grow from '@material-ui/core/Grow';
-import useChat from 'hooks/useChat';
 import Content from 'templates/Content';
 import Transition from 'components/Transition';
-import Form from 'routes/Explore/Form';
 import Bot from 'routes/Explore/Bot';
-import Chat from 'routes/Explore/Chat';
 
 export default function Explore() {
-  const [{ streamUrl }, startChat, sendMessage] = useChat();
-  const [messages, setMessages] = useState([]);
-
-  const disabled = !streamUrl;
-
   return (
     <Transition in component={Grow}>
       <Content align="center" maxWidth="md">
-        <Bot startChat={startChat} streamUrl={streamUrl} setMessages={setMessages} />
-        <Chat messages={messages} />
-        <Form sendMessage={sendMessage} disabled={disabled} />
+        <Bot />
       </Content>
     </Transition>
   );
