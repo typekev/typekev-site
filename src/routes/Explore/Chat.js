e056d1d02de02ff38507590556145c21fc837cfb
Kevin Gonzalez <typekev@gmail.com>
2019-10-19 23:21:53 +0200

    Refactor Chat to handle more features

--- src/routes/Explore/Chat.js
+++ src/routes/Explore/Chat.js
@@ -1,25 +1,112 @@
-import React, { useMemo } from 'react';
+import React, { useState, useEffect, useRef, useMemo } from 'react';
+import PropTypes from 'prop-types';
 import Keyboard, { Cursor } from 'react-mk';
+import Fade from '@material-ui/core/Fade';
+import debounce from 'lodash.debounce';
+import Transition from 'components/Transition';
 import Title from 'components/Title';
+import TypingIndicator from 'components/TypingIndicator';
+import { initialState } from 'hooks/useSocket';
 
-export const botIntroText = [
-  // 400,
-  'Welcome, visitor',
-  // "I'm Kevin's autonomous assistant",
-  // 'What can I help you with?',
-];
+export const delay = 3000;
+export const defaultSentenceDelayRange = [50, 75];
+export const minSentenceVisibilityDurationRange = [1000, 1200];
+export const getSentenceDelayRange = messageLength =>
+  messageLength * defaultSentenceDelayRange[0] > minSentenceVisibilityDurationRange[0]
+    ? defaultSentenceDelayRange
+    : minSentenceVisibilityDurationRange.map(time => time / messageLength);
 
-export const handleTyping = messages => ({ type }) =>
-  messages.length > 0 ? type(...messages) : type(...botIntroText);
+export const handleTyping = messages => ({ type }) => type(...messages);
 
-export default function Chat({ messages }) {
-  return useMemo(
-    () => (
-      <Title variant="h6" align="center">
-        <Keyboard>{handleTyping(messages)}</Keyboard>
-        <Cursor />
-      </Title>
-    ),
-    [messages],
+export const getDelay = disabled => (disabled ? 0 : delay);
+
+export const getShouldDisplayChat = (shouldDisplayChat, displayingInitialMessage) =>
+  shouldDisplayChat || displayingInitialMessage;
+
+export default function Chat({ messages, disabled }) {
+  const [debouncedMessages, setDebouncedMessages] = useState(initialState);
+  const [shouldDisplayChat, setShouldDisplayChat] = useState(initialState);
+
+  const messagesRef = useRef(messages);
+  messagesRef.current = messages;
+
+  const debouncedMessagesRef = useRef(debouncedMessages);
+  debouncedMessagesRef.current = debouncedMessages;
+
+  const disabledRef = useRef(disabled);
+  disabledRef.current = disabled;
+
+  const receivingInitialMessage = messagesRef.current[0] === initialState[0];
+  const displayingInitialMessage = debouncedMessagesRef.current[0] === initialState[0];
+
+  useEffect(() => {
+    if (!receivingInitialMessage) {
+      debounce(
+        /* istanbul ignore next */
+        () => setDebouncedMessages(messagesRef.current),
+        getDelay(disabledRef.current),
+      )();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [messages]);
+
+  useEffect(() => {
+    debounce(
+      /* istanbul ignore next */
+      () =>
+        setShouldDisplayChat(
+          debouncedMessagesRef.current.filter(Boolean).length > 0 && !disabledRef.current,
+        ),
+      getDelay(disabledRef.current),
+    )();
+  });
+
+  return (
+    <Title variant="h6" align="center">
+      <Transition
+        component={Fade}
+        in={getShouldDisplayChat(shouldDisplayChat, displayingInitialMessage)}
+        timeout={delay / 2}
+        delay={0}
+      >
+        <div>
+          {useMemo(
+            () => (
+              <Keyboard
+                sentenceDelayPerCharRange={
+                  /* istanbul ignore next */
+                  disabled && !displayingInitialMessage
+                    ? [0, 0]
+                    : getSentenceDelayRange(
+                        Math.min(
+                          ...debouncedMessages.map(message => message.length).filter(Boolean),
+                        ),
+                      )
+                }
+                keyPressDelayRange={[75, 100]}
+              >
+                {handleTyping(debouncedMessages)}
+              </Keyboard>
+            ),
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+            [debouncedMessages],
+          )}
+          <Cursor />
+        </div>
+      </Transition>
+      <Transition
+        component={Fade}
+        in={!getShouldDisplayChat(shouldDisplayChat, displayingInitialMessage)}
+        timeout={delay / 2}
+        delay={0}
+      >
+        <TypingIndicator />
+      </Transition>
+    </Title>
   );
 }
+
+Chat.propTypes = {
+  messages: PropTypes.arrayOf(PropTypes.string).isRequired,
+  disabled: PropTypes.bool.isRequired,
+};
--- src/routes/Explore/tests/Chat.test.js
+++ src/routes/Explore/tests/Chat.test.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Chat, { handleTyping, botIntroText } from 'routes/Explore/Chat';
+import Chat, {
+  handleTyping,
+  delay,
+  defaultSentenceDelayRange,
+  minSentenceVisibilityDurationRange,
+  getSentenceDelayRange,
+  getDelay,
+  getShouldDisplayChat,
+} from 'routes/Explore/Chat';
+import { initialState } from 'hooks/useSocket';
 
 describe('Chat component', () => {
   it('renders without crashing', () => {
@@ -8,9 +17,35 @@ describe('Chat component', () => {
     ReactDOM.render(<Chat messages={[]} handleTyping={handleTyping} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
+
   it('calls type and returns messages if messages.length > 0 else returns botIntroText', () => {
     const messages = ['Test'];
-    handleTyping(messages)({ type: (...args) => expect(...args).toBe(...messages) });
-    handleTyping([])({ type: (...args) => expect(...args).toBe(...botIntroText) });
+    handleTyping(messages)({ type: (...args) => expect(...args).toEqual(...messages) });
+    handleTyping([])({
+      type: (...args) => expect(...args).toEqual(...initialState.filter(Number.isNaN)),
+    });
+  });
+
+  it('returns defaultSentenceDelayRange or a calculated range based on messageLength', () => {
+    const longSentenceDelayRange = getSentenceDelayRange(100);
+    expect(longSentenceDelayRange).toEqual(defaultSentenceDelayRange);
+    const shortSentenceDelayRange = getSentenceDelayRange(1);
+    expect(shortSentenceDelayRange).toEqual(
+      minSentenceVisibilityDurationRange.map(time => time / 1),
+    );
+  });
+
+  it('returns a delay if disable is falsy else returns 0', () => {
+    const disabledDelay = getDelay(true);
+    expect(disabledDelay).toBe(0);
+    const enabledDelay = getDelay(false);
+    expect(enabledDelay).toBe(delay);
+  });
+
+  it('returns true if shouldDisplayChat or displayingInitialMessage are truthy', () => {
+    const shouldNotDisplayChat = getShouldDisplayChat(false, false);
+    expect(shouldNotDisplayChat).toBe(false);
+    const shouldDisplayChat = getShouldDisplayChat(true, false);
+    expect(shouldDisplayChat).toBe(true);
   });
 });
