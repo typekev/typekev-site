2b581220b92e36d0c5d9e177337a2ca6002c67be
Kevin Gonzalez <typekev@gmail.com>
2019-11-19 17:50:12 +0100

    content should be able to align left

--- src/templates/Content/index.js
+++ src/templates/Content/index.js
@@ -2,11 +2,19 @@ import styled, { css } from 'styled-components';
 import Container from '@material-ui/core/Container';
 
 const Content = styled(Container)`
+  margin-top: 3rem;
+  margin-bottom: 6rem;
   ${({ align }) =>
     align === 'center' &&
     css`
       align-self: center;
       margin-top: calc(-25vmin - 2rem);
+    `}
+
+  ${({ align }) =>
+    align === 'left' &&
+    css`
+      max-width: 75%;
     `}
 `;
 
