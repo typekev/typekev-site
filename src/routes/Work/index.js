0b636f7f98879076da528df77dc202fc4508fab4
Kevin Gonzalez <typekev@gmail.com>
2019-11-19 17:54:41 +0100

    Add title to Work

--- src/routes/Work/index.js
+++ src/routes/Work/index.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import Grow from '@material-ui/core/Grow';
+import Typography from '@material-ui/core/Typography';
+import Keyboard, { Cursor } from 'react-mk';
+import Content from 'templates/Content';
+import Transition from 'components/Transition';
+import InvertedText from 'components/InvertedText';
 
 export default function Work() {
-  return <div>Work</div>;
+  return (
+    <Transition in component={Grow}>
+      <Content fixed align="left">
+        <Typography variant="h5">
+          <InvertedText align="left">
+            <Keyboard>I am a #DigitalTransformaker</Keyboard>
+            <Cursor />
+          </InvertedText>
+        </Typography>
+        <br />
+        <br />
+        <Typography variant="body1">Work</Typography>
+      </Content>
+    </Transition>
+  );
 }
