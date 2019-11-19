8b40f087f83da97551c42f9d0e7bd5c9b58e89c6
Kevin Gonzalez <typekev@gmail.com>
2019-11-19 17:54:13 +0100

    Add title to Blog

--- src/routes/Blog/index.js
+++ src/routes/Blog/index.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import Grow from '@material-ui/core/Grow';
+import Typography from '@material-ui/core/Typography';
+import Keyboard, { Cursor } from 'react-mk';
+import Content from 'templates/Content';
+import Transition from 'components/Transition';
+import InvertedText from 'components/InvertedText';
 
 export default function Blog() {
-  return <div>Blog</div>;
+  return (
+    <Transition in component={Grow}>
+      <Content fixed align="left">
+        <Typography variant="h5">
+          <InvertedText align="left">
+            <Keyboard>I am a writer</Keyboard>
+            <Cursor />
+          </InvertedText>
+        </Typography>
+        <br />
+        <br />
+        <Typography variant="body1">Blog</Typography>
+      </Content>
+    </Transition>
+  );
 }
