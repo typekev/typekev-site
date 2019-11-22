349af16c48452cc51e39d4e9ea39f86f79a8d78f
Kevin Gonzalez <typekev@gmail.com>
2019-11-22 18:19:35 +0100

    Shorten useSocket initialState CTA

--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -2,13 +2,14 @@ import { useState, useEffect, useRef } from 'react';
 
 const sampleQuestions = ['Who is Kevin?', 'What does Kevin do?', 'Where does Kevin work?'];
 
-const getSampleQuestion = () => sampleQuestions[Math.floor(Math.random() * sampleQuestions.length)];
+export const getSampleQuestion = () =>
+  sampleQuestions[Math.floor(Math.random() * sampleQuestions.length)];
 
 export const initialState = [
   1000,
   'Welcome, visitor',
-  "I'm Kevin's autonomous assistant",
-  `What can I help you with? You can ask me something like '${getSampleQuestion()}'`,
+  "I'm Kevin's autonomous assistant.",
+  `Ask me something like '${getSampleQuestion()}`,
 ];
 
 export const onMessageReceived = (messagesRef, setMessages) => ({ data }) =>
