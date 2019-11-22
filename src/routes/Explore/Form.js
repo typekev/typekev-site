b5f1bb094bf8a19ec027bd5af60e9f83276189d1
Kevin Gonzalez <typekev@gmail.com>
2019-11-22 18:20:12 +0100

    Add submit and generate question button to Form

--- src/routes/Explore/Form.js
+++ src/routes/Explore/Form.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import TextField from '@material-ui/core/TextField';
+import FilledInput from '@material-ui/core/FilledInput';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from '@material-ui/core/IconButton';
+import Icon from '@mdi/react';
+import { mdiSend, mdiRobot } from '@mdi/js';
+import { Container, Tooltip } from '@material-ui/core';
+import { getSampleQuestion } from 'hooks/useSocket';
 
 export const initialState = {
   value: '',
@@ -18,20 +24,50 @@ export default function Form({ sendMessage, disabled }) {
   const [value, setValue] = useState(initialState.value);
 
   return (
-    <form onSubmit={onSubmit(sendMessage, setValue, value)}>
-      <TextField
-        onChange={onChange(setValue)}
-        value={value}
-        disabled={disabled}
-        id="chatbot-input"
-        margin="dense"
-        autoFocus
-        hiddenLabel
-        variant="filled"
-        placeholder={disabled ? 'The bot is typing...' : 'Tell me something...'}
-        inputProps={{ 'aria-label': 'chatbot input' }}
-      />
-    </form>
+    <Container maxWidth="sm">
+      <form onSubmit={onSubmit(sendMessage, setValue, value)}>
+        <FilledInput
+          fullWidth
+          autoFocus
+          hiddenLabel
+          onChange={onChange(setValue)}
+          value={value}
+          disabled={disabled}
+          id="chatbot-input"
+          margin="dense"
+          placeholder={disabled ? 'The bot is typing' : 'Tell me something'}
+          inputProps={{ 'aria-label': 'chatbot input' }}
+          startAdornment={
+            <InputAdornment position="start">
+              <Tooltip title="Generate a random question">
+                <IconButton
+                  edge="start"
+                  aria-label="generate a random question"
+                  onClick={() => setValue(getSampleQuestion())}
+                  onMouseDown={() => setValue(getSampleQuestion())}
+                >
+                  <Icon path={mdiRobot} size={1} color="currentColor" />
+                </IconButton>
+              </Tooltip>
+            </InputAdornment>
+          }
+          endAdornment={
+            <InputAdornment position="end">
+              <Tooltip title="Submit">
+                <IconButton
+                  edge="end"
+                  aria-label="submit message"
+                  onClick={onSubmit(sendMessage, setValue, value)}
+                  onMouseDown={onSubmit(sendMessage, setValue, value)}
+                >
+                  <Icon path={mdiSend} size={1} color="currentColor" />
+                </IconButton>
+              </Tooltip>
+            </InputAdornment>
+          }
+        />
+      </form>
+    </Container>
   );
 }
 
