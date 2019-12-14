21748d80a14da2e2a4a4611e0f6f7b14741c1179
Kevin Gonzalez <typekev@gmail.com>
2019-12-14 21:30:34 +0100

    Add Devoteam logo

---
+++ src/components/DevoteamLogo/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function DevoteamLogo() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="258.7" height="258.7" viewBox="0 0 258.7 258.7">
+      <g>
+        <path
+          fill="currentColor"
+          d="M129.3.2c71.3 0 129.1 57.8 129.1 
+          129.1s-57.8 129.1-129.1 129.1-129.1-57.8-129.1-129c0-71.4 
+          57.8-129.2 129.1-129.2zm86.1 
+          119.5v-30.5h-49.5v-46h-31.7v135.8h-26.1c-18.9 0-28.7-11.1-28.7-29.5 0-19.5 
+          10.5-29.5 28.7-29.5h20.4c-2.1-16-7.5-30.9-7.5-30.9h-13.7c-44.5 0-60.4 
+          29.9-60.4 60.4 0 41.2 28.7 60.4 60.4 60.4h58.6v-90.3h49.5z"
+        />
+      </g>
+    </svg>
+  );
+}
+
+DevoteamLogo.propTypes = {};
+
+export default DevoteamLogo;
