064549767767d733da3b6b1740264f6b6deda708
Kevin Gonzalez <typekev@gmail.com>
2019-12-14 21:32:58 +0100

    Adjust content of Discover page

--- src/routes/Discover/index.js
+++ src/routes/Discover/index.js
@@ -12,6 +12,10 @@ import Link from 'components/Link';
 const CURRENT_DATE = new Date();
 const CURRENT_MONTH_IN_YEAR = CURRENT_DATE.getMonth();
 const START_DATE = new Date(new Date().setFullYear(2013));
+const yearsCounter =
+  CURRENT_DATE.getFullYear() -
+  START_DATE.getFullYear() +
+  Math.round((CURRENT_MONTH_IN_YEAR / 12) * 10) / 10;
 
 export default function Discover() {
   return (
@@ -29,14 +33,16 @@ export default function Discover() {
           Hi, I&apos;m a full stack software engineer, and <i>I really like technology.</i>
           <br />
           <br />I have explored, and continue to explore all the technology that interests me: from
-          front-end web and mobile development with React; to blockchain engineering with
-          Hyperledger; to building machine learning models with Python; to architecting cloud and
-          serverless solutions on Azure, Firebase, and AWS. Over the last
-          {` ${CURRENT_DATE.getFullYear() -
-            START_DATE.getFullYear() +
-            Math.round((CURRENT_MONTH_IN_YEAR / 12) * 10) / 10} `}
-          years I have developed a deep understanding of computer science in both theory and
-          practical application.
+          front-end web and mobile development with <A href="https://reactjs.org">React</A> (for
+          instance <A href="https://github.com/typekev/typekev-site">this website</A> that
+          you&apos;re using right noe was made with React!); to blockchain engineering with
+          <A href="https://www.hyperledger.org/">Hyperledger</A>; to building machine learning
+          models with <A href="https://www.python.org/">Python</A>; to architecting cloud and
+          serverless solutions on <A href="https://azure.microsoft.com/en-us/">Azure</A>,{' '}
+          <A href="https://firebase.google.com/">Firebase</A>, and{' '}
+          <A href="https://aws.amazon.com/">AWS</A>. Over the last {yearsCounter} years I have
+          developed a deep understanding of computer science in both theory and practical
+          application.
           <br />
           <br />I want to do more than simply write code. I want to write history. Therefore, I
           advocate for clean code, agile development, and innovative technologies.
@@ -51,11 +57,9 @@ export default function Discover() {
         </Typography>
         <br />
         <br />
-        <Link to="/work">
-          <Button variant="contained" color="secondary">
-            Click here to learn more about what I do
-          </Button>
-        </Link>
+        <Button component={Link} to="/work" variant="contained" color="secondary">
+          Click here to learn more about what I do
+        </Button>
       </Content>
     </Transition>
   );
