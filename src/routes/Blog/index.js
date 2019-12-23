e0af4b74dcbaaf47a714318af965a7fc5341b743
Kevin Gonzalez <typekev@gmail.com>
2019-12-23 17:30:01 +0100

    Add content to blog route

--- src/routes/Blog/index.js
+++ src/routes/Blog/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Grow from '@material-ui/core/Grow';
+import Fade from '@material-ui/core/Fade';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import Keyboard, { Cursor } from 'react-mk';
+import useBlog from 'hooks/useBlog';
 import Content from 'templates/Content';
 import Transition from 'components/Transition';
 import InvertedText from 'components/InvertedText';
+import Link from 'components/Link';
+import { ButtonGroup } from '@material-ui/core';
 
 export default function Blog() {
+  const [posts] = useBlog();
+
   return (
     <Transition in component={Grow}>
       <Content fixed align="left">
@@ -18,7 +25,31 @@ export default function Blog() {
         </Typography>
         <br />
         <br />
-        <Typography variant="body1">Blog</Typography>
+        {useMemo(
+          () =>
+            Object.values(posts)
+              .sort((a, b) => (a.timestamp > b.timestamp ? -1 : 1))
+              .map(({ title, published }, index) => {
+                const delay = (index + 1) * 800;
+                return (
+                  <div key={title}>
+                    <Transition in component={Fade} delay={delay} timeout={1000}>
+                      <ButtonGroup variant="outlined" color="inherit">
+                        <Button disabled>{published}</Button>
+                        <Button component={Link}>
+                          <Keyboard keyPressDelayRange={[40, 90]}>
+                            {({ type }) => type(delay, title)}
+                          </Keyboard>
+                        </Button>
+                      </ButtonGroup>
+                    </Transition>
+                    <br />
+                    <br />
+                  </div>
+                );
+              }),
+          [posts],
+        )}
       </Content>
     </Transition>
   );
