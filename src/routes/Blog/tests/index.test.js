e5b33b68df816d768ce8194ea855724b292eb91b
Kevin Gonzalez <typekev@gmail.com>
2019-12-25 00:52:02 +0100

    Add extensive tests for Blog

--- src/routes/Blog/tests/index.test.js
+++ src/routes/Blog/tests/index.test.js
@@ -5,22 +5,49 @@ import Blog, { compare, sortPosts, getDelay, renderPosts } from 'routes/Blog';
 import { Button } from '@material-ui/core';
 
 const posts = [
-  { title: 'Title 1', published: 'Date', timestamp: 1 },
-  { title: 'Title 1', published: 'Date', timestamp: 2 },
+  { title: 'Title 1', content: 'content', published: 'Date', timestamp: 1 },
+  { title: 'Title 1', content: 'content', published: 'Date', timestamp: 2 },
 ];
 
 const setPostId = jest.fn();
+const push = jest.fn();
 
 describe('Blog route', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Blog match={{ params: { postId: 'post.md.json' } }} />, div);
+
+    ReactDOM.render(
+      <Blog
+        history={{ push }}
+        match={{ params: { postId: 'post.md.json' } }}
+        initialPost={posts[0]}
+      />,
+      div,
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Blog match={{ params: {} }} />, div);
+
+    ReactDOM.render(
+      <Blog history={{ push }} match={{ params: { postId: 'post.md.json' } }} initialPost={{}} />,
+      div,
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+
+    ReactDOM.render(<Blog history={{ push }} match={{ params: {} }} initialPost={posts[0]} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+
+    ReactDOM.render(<Blog history={{ push }} match={{ params: {} }} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
@@ -51,6 +78,20 @@ describe('Blog route', () => {
     const wrapper = shallow(<Posts />);
     const button = wrapper.find(Button).at(1);
     button.props().onClick();
+    expect(setPostId.mock.calls.length).toEqual(1);
+  });
+
+  it('triggers a onClick event within the return of renderPosts, triggering setPostId', () => {
+    const wrapper = shallow(
+      <Blog
+        history={{ push }}
+        match={{ params: { postId: 'post.md.json' } }}
+        initialPost={posts[0]}
+      />,
+    );
+    const button = wrapper.find(Button).at(1);
+    button.props().onClick();
+    expect(push.mock.calls.length).toEqual(1);
     expect(setPostId.mock.calls.length).toEqual(1);
   });
 });
