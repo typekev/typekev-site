a3e72c8c243b4a9c463787831db959531f0e84a2
Kevin Gonzalez <typekev@gmail.com>
2019-12-24 23:12:27 +0100

    Update getDelay test

--- src/routes/Blog/tests/index.test.js
+++ src/routes/Blog/tests/index.test.js
@@ -24,8 +24,8 @@ describe('Blog route', () => {
   });
 
   it('returns a delay based on the formula (index + 1) * 800', () => {
-    expect(getDelay(0)).toEqual(800);
-    expect(getDelay(9)).toEqual(8000);
+    expect(getDelay(1)).toEqual(800);
+    expect(getDelay(10)).toEqual(8000);
   });
 
   it('renders without crashing', () => {
