deab8387bcbc85dca8a1e0fe3e1d359c5c129b5f
Kevin Gonzalez <typekev@gmail.com>
2019-12-25 00:03:30 +0100

    test for getBlogPosts expects undefined

--- src/routes/Blog/api/tests/index.test.js
+++ src/routes/Blog/api/tests/index.test.js
@@ -2,7 +2,7 @@ import { getBlogPosts } from 'routes/Blog/api';
 
 describe('Blog route APIs', () => {
   it('gets a JSON representation of blog posts', () => {
-    getBlogPosts().then(res => expect(typeof res).toEqual('object'));
+    getBlogPosts().then(res => expect(typeof res).toEqual('undefined'));
 
     expect(fetch.mock.calls.length).toEqual(1);
   });
