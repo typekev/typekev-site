62190eb84f2c6216e7c30b4a293df9d63d6c0521
Kevin Gonzalez <typekev@gmail.com>
2019-12-24 23:04:11 +0100

    Refactor InvertedText component to be a disabled MuiButton

--- src/components/InvertedText/index.js
+++ src/components/InvertedText/index.js
@@ -1,26 +1,34 @@
+import React from 'react';
 import styled, { css } from 'styled-components';
-import { withTheme } from '@material-ui/core';
+import { withTheme } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
-const InvertedText = styled.span`
+const StyledButton = styled(Button)`
   ${({
     theme: {
       palette: { background, primary },
     },
   }) => css`
-    background-color: ${primary.dark};
-    color: ${background.default};
+    background-color: ${primary.dark} !important;
+    border-color: ${primary.dark} !important;
+    color: ${background.default} !important;
   `}
-  padding: 0.5rem 1rem;
-  border-radius: 0.25rem;
-  line-height: 2rem;
-  display: inline-flex;
-  align-items: flex-end;
 
-  ${({ align }) =>
-    align === 'left' &&
-    css`
-      margin-left: -1rem;
-    `}
+  text-align: left;
+  display: flow-root !important;
 `;
+
+function InvertedText({ ...rest }) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return (
+    <StyledButton
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...rest}
+      disabled
+      variant="outlined"
+      size="large"
+    />
+  );
+}
 
 export default withTheme(InvertedText);
