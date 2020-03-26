f2689d630ddc043216770a6bb6424524f6586c2b
Kevin Gonzalez <typekev@gmail.com>
2020-03-26 22:26:43 +0100

    drop useless catch (error catches and logs)

--- src/hooks/useBlog.js
+++ src/hooks/useBlog.js
@@ -3,23 +3,10 @@ import { getBlogPosts, getBlogPost } from 'routes/Blog/api';
 
 export const initialState = {};
 
-export const getPosts = async setPosts => {
-  try {
-    const posts = (await getBlogPosts()) || initialState;
-    setPosts(posts);
-  } catch (err) {
-    setPosts(initialState);
-  }
-};
+export const getPosts = async setPosts => setPosts((await getBlogPosts()) || initialState);
 
-export const getSinglePost = setPost => async location => {
-  try {
-    const post = (location && (await getBlogPost(location))) || initialState;
-    setPost(post);
-  } catch (err) {
-    setPost(initialState);
-  }
-};
+export const getSinglePost = setPost => async location =>
+  setPost((location && (await getBlogPost(location))) || initialState);
 
 export default function useBlog(initialPost) {
   const [posts, setPosts] = useState(initialState);
