08331aceff77a140c3ef056e77ad24967ac07f68
Kevin Gonzalez <typekev@gmail.com>
2020-05-03 23:17:13 +0200

    Add project content container

---
+++ src/routes/Projects/ProjectContent.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import Container from '@material-ui/core/Container';
+
+const ProjectContent = styled(Container)`
+  height: calc(100% - 4.5rem);
+
+  & > div {
+    border-radius: 0.25rem;
+  }
+
+  & iframe {
+    border: none;
+    border-radius: 0.25rem;
+  }
+`;
+
+export default ProjectContent;
