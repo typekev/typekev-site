c1d8adcca50806b67391f1b09c49328558242f99
Kevin Gonzalez <typekev@gmail.com>
2020-07-29 23:54:41 +0200

    typeo out title

--- src/components/Header/HeaderText.js
+++ src/components/Header/HeaderText.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Keyboard from 'react-mk';
 
 export default function HeaderText() {
   return (
     <>
-      <strong>typekev</strong> | software engineer
+      <strong>typekev</strong> | <Keyboard>{({ type }) => type(500, 'software engineer')}</Keyboard>
     </>
   );
 }
