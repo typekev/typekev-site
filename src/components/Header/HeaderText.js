aa88f72a04f16158376f3440e1368500bb479be5
Kevin Gonzalez <typekev@gmail.com>
2020-07-30 23:20:51 +0200

    Add typing effect to title

--- src/components/Header/HeaderText.js
+++ src/components/Header/HeaderText.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import Keyboard from 'react-mk';
+import React, { useState, useEffect } from 'react';
+import Keyboard, { Cursor } from 'react-mk';
+
+const typeNames = ({ type }) => type(1000, 'Kevin Gonzalez ', 'typekev ');
+const typeTitles = ({ type }) => type(3500, ' husband', ' dad', ' leader', ' software engineer');
 
 export default function HeaderText() {
+  const [blink, setBlink] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setBlink(false), 20000);
+  }, []);
+
   return (
     <>
-      <strong>typekev</strong> | <Keyboard>{({ type }) => type(500, 'software engineer')}</Keyboard>
+      <strong>
+        <Keyboard keyPressDelayRange={[70, 80]} sentenceDelayPerCharRange={[600, 600]}>
+          {typeNames}
+        </Keyboard>
+      </strong>
+      <Cursor blink={blink} />
+      <Keyboard keyPressDelayRange={[60, 70]} sentenceDelayPerCharRange={[120, 150]}>
+        {typeTitles}
+      </Keyboard>
     </>
   );
 }
