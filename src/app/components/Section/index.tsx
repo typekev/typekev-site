51887564c1a68d45ca1e0068c893b99e89a46f99
Kevin Gonzalez <typekev@gmail.com>
2021-10-25 03:43:29 +0200

    Adds breakpoints to Section

--- src/app/components/Section/index.tsx
+++ src/app/components/Section/index.tsx
@@ -6,6 +6,8 @@
 import { memo, PropsWithChildren, ReactNode } from 'react';
 import styled from 'styled-components/macro';
 
+import { media } from 'styles/media';
+
 interface Props {
   title?: ReactNode;
 }
@@ -20,12 +22,21 @@ export const Section = memo(({ title, children }: PropsWithChildren<Props>) => {
 });
 
 const SectionTitle = styled.div`
-  font-size: 1.5em;
+  font-size: 1.25em;
   line-height: 1;
   margin-top: 3em;
-  margin-left: 0.25em;
+  margin-left: 1px;
+
+  ${media.small`
+    font-size: 1.5em;
+    margin-left: 4px;
+  `}
 `;
 
 const SectionContent = styled.div`
-  font-size: 3em;
+  font-size: 2em;
+
+  ${media.small`
+    font-size: 3em;
+  `}
 `;
