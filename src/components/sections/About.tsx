ccee13366d3bf0e11ab9b7f063aa0684d76a348f
Kevin Gonzalez <kevin.gonzalez@ses.com>
2023-12-28 02:36:24 +0100

    Removes nasadings icon

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,15 +1,11 @@
-import localFont from "next/font/local";
-
 import { useTranslations } from "next-intl";
-
-const nasaDings = localFont({ src: "./about/nasadings.ttf" });
 
 export default function About() {
   const t = useTranslations("About");
 
   return (
     <section>
-      <b className={`${nasaDings.className} logo`} data-text="K">
+      <b className="logo" data-text="K">
         K
       </b>
       <span>
