c163072573e500694e6b26b3659a536b10d86936
Kevin Gonzalez <kevin.gonzalez@ses.com>
2024-04-24 21:21:49 +0200

    Updates chatbot to load with dynamic

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,16 +1,15 @@
-"use client";
 import dynamic from "next/dynamic";
 import { Suspense } from "react";
 
-import { Chatbot } from "@/components/Chatbot";
 import About from "@/components/sections/About";
 import Contact from "@/components/sections/Contact";
 import Endnote from "@/components/sections/Endnote";
 import Work from "@/components/sections/Work";
 
-const BlackHole = dynamic(() => import("@/components/canvas/BlackHole").then((mod) => mod.BlackHole), { ssr: false });
 const View = dynamic(() => import("@/components/canvas/View").then((mod) => mod.View), { ssr: false });
+const BlackHole = dynamic(() => import("@/components/canvas/BlackHole").then((mod) => mod.BlackHole), { ssr: false });
 const Common = dynamic(() => import("@/components/canvas/View").then((mod) => mod.Common), { ssr: false });
+const Chatbot = dynamic(() => import("@/components/Chatbot").then((mod) => mod.Chatbot), { ssr: false });
 
 export default function Page() {
   return (
