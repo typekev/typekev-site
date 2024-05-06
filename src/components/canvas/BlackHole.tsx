983e8e10419c5e4c6797b195af72064cdadd64cc
Kevin Gonzalez <kevin.gonzalez@ses.com>
2024-05-06 23:11:06 +0200

    Improves blackhole scale and position on mobile

--- src/components/canvas/BlackHole.tsx
+++ src/components/canvas/BlackHole.tsx
@@ -6,11 +6,15 @@ import { PrimitiveProps, useFrame } from "@react-three/fiber";
 
 import { useBloom } from "@/templates/hooks/usePostprocess";
 
+const scrollMin = -0.25 as const;
+const scrollMax = 0.2 as const;
+
 export function BlackHole(props: Omit<PrimitiveProps, "object">) {
   const { scene } = useGLTF("/black-hole.glb");
 
+  const smallScreen = useMemo(() => window.innerWidth < 768, [window.innerWidth]);
   const scrollHeight = useMemo(() => document.documentElement.scrollHeight * 1.5, []);
-  const inverseScroll = useCallback((scrollY: number) => Math.max(0.17 - scrollY / scrollHeight, -0.22), []);
+  const inverseScroll = useCallback((scrollY: number) => Math.max(scrollMax - scrollY / scrollHeight, scrollMin), []);
 
   const easeInOutQuad = useCallback((t: number) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t), []);
   const ease = (scroll: number) => Math.min(easeInOutQuad(Math.abs(scroll - scene.rotation.x)), 0.1);
@@ -19,11 +23,16 @@ export function BlackHole(props: Omit<PrimitiveProps, "object">) {
   const getPositionY = useCallback((y: number, scroll: number) => y + (y > -scroll ? -ease(scroll) : ease(scroll)), []);
 
   useBloom();
+
   useEffect(() => {
-    scene.position.setX(0.5);
-    scene.scale.set(0.6, 0.6, 0.6);
+    const positionX = smallScreen ? 0.3 : 0.5;
+    scene.position.setX(positionX);
+
+    const scale = smallScreen ? 0.5 : 0.6;
+    scene.scale.set(scale, scale, scale);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   useFrame((_, delta) => {
     scene.rotation.y -= delta * 4;
     const scroll = inverseScroll(window.scrollY);
