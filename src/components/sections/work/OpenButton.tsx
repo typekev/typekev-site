0e3d6d53f84fb866a8e9d629beaa0ad8150b162f
Kevin Gonzalez <kevin.gonzalez@ses.com>
2024-05-20 01:05:46 +0200

    Adds years to working history

--- src/components/sections/work/OpenButton.tsx
+++ src/components/sections/work/OpenButton.tsx
@@ -7,7 +7,7 @@ interface Props {
 export function OpenButton({ href }: Props) {
   return (
     <a href={href} className="button icon-button" aria-label="Navigate to associated workplace's website">
-      <ArrowUpRightIcon size="1em" strokeWidth={2.25} />
+      <ArrowUpRightIcon size="0.875em" strokeWidth={1.5} />
     </a>
   );
 }
