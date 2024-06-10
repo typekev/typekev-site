550cc923a86bcebe4fe3b0f5373b6e5b46f4e65d
Kevin Gonzalez <kevin.gonzalez@ses.com>
2024-06-10 10:11:40 +0200

    Adds projects listing under work

--- src/components/sections/Work.tsx
+++ src/components/sections/Work.tsx
@@ -1,9 +1,10 @@
 import { useTranslations } from "next-intl";
 
 import { AnchorButton } from "@/components/AnchorButton";
-import { devoteamUrl, emailtreeUrl, linkedInExperienceUrl, sesUrl } from "@/helpers/links";
+import { devoteamUrl, emailtreeUrl, githubTypekevSiteUrl, linkedInExperienceUrl, sesUrl } from "@/helpers/links";
 
 import { OpenButton } from "./work/OpenButton";
+import { FlaskConicalIcon, Gamepad2Icon, KeyboardIcon } from "lucide-react";
 
 export default function Work() {
   const t = useTranslations("Work");
@@ -38,6 +39,19 @@ export default function Work() {
         <li>
           <AnchorButton href={linkedInExperienceUrl}>{t("more")}</AnchorButton>
           <p className="work-year">&nbsp;&mdash; 2017</p>
+        </li>
+        <li className="button-group">
+          <AnchorButton href={githubTypekevSiteUrl}>Projects</AnchorButton>
+          <p className="work-year">Open-source</p>
+          <a href="#" className="button icon-button" aria-label="Navigate to a react library I made">
+            <KeyboardIcon size="0.875em" strokeWidth={1.5} />
+          </a>
+          <a href="#" className="button icon-button" aria-label="Navigate to my authored Python courses">
+            <FlaskConicalIcon size="0.875em" strokeWidth={1.5} />
+          </a>
+          <a href="#" className="button icon-button" aria-label="Navigate to a browser game I made">
+            <Gamepad2Icon size="0.875em" strokeWidth={1.5} />
+          </a>
         </li>
       </ul>
     </section>
