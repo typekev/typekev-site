10ba8117a0e621c0d3cbbe78bcf9825904d75700
Kevin Gonzalez <19812642+typekev@users.noreply.github.com>
2024-10-12 18:25:30 +0200

    Improves opening animation and loading order

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -4,24 +4,27 @@ import About from "@/components/sections/About";
 import Contact from "@/components/sections/Contact";
 import Endnote from "@/components/sections/Endnote";
 import Work from "@/components/sections/Work";
+import { Chatbot } from "@/components/Chatbot";
 
 const View = dynamic(() => import("@/components/canvas/View").then((mod) => mod.View), { ssr: false });
 const BlackHole = dynamic(() => import("@/components/canvas/BlackHole").then((mod) => mod.BlackHole), { ssr: false });
 const Common = dynamic(() => import("@/components/canvas/View").then((mod) => mod.Common), { ssr: false });
-const Chatbot = dynamic(() => import("@/components/Chatbot").then((mod) => mod.Chatbot), { ssr: false });
 
 export default function Page() {
   return (
-    <main>
-      <View orbit className="black-hole">
-        <BlackHole />
-        <Common />
-      </View>
-      <About />
-      <Work />
-      <Contact />
-      <Chatbot />
-      <Endnote />
-    </main>
+    <>
+      <aside className="overlay" />
+      <main>
+        <View orbit className="black-hole">
+          <BlackHole />
+          <Common />
+        </View>
+        <About />
+        <Work />
+        <Contact />
+        <Chatbot />
+        <Endnote />
+      </main>
+    </>
   );
 }
