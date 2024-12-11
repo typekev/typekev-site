da12c2e2c879ff1f13cfa222d6a495b417dba2ba
Kevin Gonzalez <19812642+typekev@users.noreply.github.com>
2024-12-11 18:54:51 +0100

    Moves scene onload logic to oncreated along with a handler for setting initial width after creation

--- src/components/canvas/Scene.tsx
+++ src/components/canvas/Scene.tsx
@@ -1,28 +1,26 @@
 "use client";
-import { useEffect } from "react";
+import { useState } from "react";
 
-import { Preload, useProgress } from "@react-three/drei";
+import { Preload } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import * as THREE from "three";
 
 import { r3f } from "@/helpers/global";
 
 export default function Scene() {
-  const { loaded } = useProgress();
-
-  useEffect(() => {
-    if (loaded) {
-      document.body.style.setProperty("--opening-transition-opacity", "0");
-      document.body.style.setProperty("--opening-play-state", "running");
-    }
-  }, [loaded]);
+  const [width, setWidth] = useState("1px");
 
   return (
     <Canvas
       className="black-hole-canvas"
-      style={{ position: "fixed", height: undefined }}
+      style={{ position: "fixed", height: undefined, width }}
       eventPrefix="client"
-      onCreated={(state) => (state.gl.toneMapping = THREE.AgXToneMapping)}
+      onCreated={(state) => {
+        state.gl.toneMapping = THREE.AgXToneMapping;
+        document.body.style.setProperty("--opening-transition-opacity", "0");
+        document.body.style.setProperty("--opening-play-state", "running");
+        setWidth("100%");
+      }}
     >
       <r3f.Out />
       <Preload all />
