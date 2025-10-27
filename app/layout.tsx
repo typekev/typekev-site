926c5531e43b0617bdcaaf6c9b375f5b1ad24601
Kevin Gonzalez <19812642+typekev@users.noreply.github.com>
2025-10-27 09:17:06 +0100

    Init v6

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,11 @@
-import { Ubuntu_Mono, Gantari } from "next/font/google";
-import { PropsWithChildren } from "react";
+import "./globals.css";
 
-import { Analytics } from "@vercel/analytics/react";
-import { getLocale } from "next-intl/server";
+type Props = Readonly<{ children: React.ReactNode }>;
 
-import { BlackHoleScene } from "@/components/canvas/BlackHoleScene";
-import "@/global.css"; // eslint-disable-line import/no-unassigned-import
-
-import { title, description } from "./head";
-
-const poppins = Gantari({
-  weight: ["100", "300", "400", "700", "800"],
-  subsets: ["latin"],
-  variable: "--font-gantari",
-});
-const fragmentMono = Ubuntu_Mono({
-  weight: ["400"],
-  subsets: ["latin"],
-  variable: "--font-ubuntu-mono",
-});
-
-export const metadata = { title, description };
-
-export default async function RootLayout({ children }: PropsWithChildren) {
-  const locale = await getLocale();
-
+export default function RootLayout({ children }: Props) {
   return (
-    <html lang={locale} className={`${fragmentMono.variable} ${poppins.variable}`}>
-      <head />
-      <body id="root">
-        <aside className="shadow-gradient" />
-        <aside className="background-gradient" />
-        <aside className="background-gradient blur" />
-        <aside className="background-gradient blur-2" />
-        {children}
-        <BlackHoleScene />
-        <Analytics />
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   );
 }
