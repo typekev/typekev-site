fa515e4920ec8359e483ed19dd0a6fce23b0aecc
Kevin Gonzalez <19812642+typekev@users.noreply.github.com>
2025-11-07 20:12:57 +0100

    Makes profile header sticky on large screens

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { Ventures } from "@/components/Ventures";
 export default function Home() {
   return (
     <>
-      <header className="flex flex-col items-center lg:items-start gap-10 lg:gap-16">
+      <header className="flex flex-col items-center lg:items-start gap-10 lg:gap-16 lg:sticky lg:top-12 lg:h-fit">
         <Profile />
       </header>
       <main className="flex flex-col gap-16">
