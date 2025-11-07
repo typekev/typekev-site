582101542be594d138bc2df893e36b0765991068
Kevin Gonzalez <19812642+typekev@users.noreply.github.com>
2025-11-07 17:31:41 +0100

    Adds short profile desc

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import { ProfileDescription } from "@/components/ProfileDescription";
+
 export default function Home() {
-  return null;
+  return (
+    <main
+      id="main-content"
+      className="mx-auto max-w-7xl px-6 py-16 md:py-24 lg:py-12 lg:grid lg:grid-cols-2 lg:gap-16 relative z-10"
+    >
+      <section className="mt-16">
+        <ProfileDescription />
+      </section>
+    </main>
+  );
 }
