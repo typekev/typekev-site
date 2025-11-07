1fe59de598b3c6ad6349c36d603134c9102743f6
Kevin Gonzalez <19812642+typekev@users.noreply.github.com>
2025-11-07 18:34:24 +0100

    Adds geist fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,18 @@
+import { Geist, Geist_Mono } from "next/font/google";
+
 import Background from "@/components/Background";
 import { ThemeToggle } from "@/components/ThemeToggle";
 import "./globals.css";
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist",
+});
+
+const geistMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-geist-mono",
+});
 
 type Props = Readonly<{ children: React.ReactNode }>;
 
@@ -10,7 +22,7 @@ export default function RootLayout({ children }: Props) {
       <head>
         <meta name="apple-mobile-web-app-title" content="keving.me" />
       </head>
-      <body>
+      <body className={`${geist.variable} ${geistMono.variable} antialiased`}>
         <ThemeToggle />
         <Background />
         {children}
