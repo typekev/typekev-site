d6b7088691eb270a5ffa170d5fc5d49659b73fe6
Kevin Gonzalez <19812642+typekev@users.noreply.github.com>
2025-11-07 17:13:16 +0100

    Adds background with animation and dark/light gradient

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Background from "@/components/Background";
 import "./globals.css";
 
 type Props = Readonly<{ children: React.ReactNode }>;
@@ -5,6 +6,7 @@ type Props = Readonly<{ children: React.ReactNode }>;
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
+      <Background />
       <body>{children}</body>
     </html>
   );
