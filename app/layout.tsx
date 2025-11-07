13827316bad6e43a5ca87eaf3492d0dd4cf83249
Kevin Gonzalez <19812642+typekev@users.noreply.github.com>
2025-11-07 17:42:24 +0100

    Updates favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ type Props = Readonly<{ children: React.ReactNode }>;
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="keving.me" />
+      </head>
       <body>
         <ThemeToggle />
         <Background />
