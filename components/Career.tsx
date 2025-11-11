872359c3036bca87e365511261fcd4edf57ab7dd
Kevin Gonzalez <19812642+typekev@users.noreply.github.com>
2025-11-11 11:24:53 +0100

    Seperates career data into its own file

--- components/Career.tsx
+++ components/Career.tsx
@@ -1,45 +1,4 @@
-const careerData = [
-  {
-    company: "Symphonee",
-    role: "Co-founder & CTO",
-    years: "2025 - Present",
-  },
-  {
-    company: "Scale Tiny",
-    role: "Founder",
-    years: "2025 - Present",
-  },
-  {
-    company: "SES Satellites",
-    role: "Yield Excellence Programs Lead",
-    years: "2022 - Present",
-  },
-  {
-    company: "EmailTree AI",
-    role: "Principal Software Engineer",
-    years: "2021 - 2022",
-  },
-  {
-    company: "Devoteam",
-    role: "Lead Software Engineer",
-    years: "2017 - 2021",
-  },
-  {
-    company: "Rise One Solutions",
-    role: "Software Engineer",
-    years: "2015 - 2017",
-  },
-  {
-    company: "Netcom",
-    role: "Engineering Manager",
-    years: "2013 - 2015",
-  },
-  {
-    company: "Perfect Smile Doc",
-    role: "Network Engineer",
-    years: "2013",
-  },
-];
+import { careerData } from "./career/careerData";
 
 export function Career() {
   return (
