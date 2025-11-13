f5a00df1b2a5b22fcaa3ff6caea3ab1b70d57d9f
Kevin Gonzalez <19812642+typekev@users.noreply.github.com>
2025-11-13 02:29:04 +0100

    Simplify connect buttons to avoid detracting from focus on the rest of the page

--- components/Connect.tsx
+++ components/Connect.tsx
@@ -1,3 +1,5 @@
+import { Send } from "lucide-react";
+
 import { Button } from "@/components/ui/button";
 
 export function Connect() {
@@ -5,36 +7,47 @@ export function Connect() {
     <section aria-labelledby="connect-heading">
       <h2
         id="connect-heading"
-        className="mb-6 text-md font-extrabold uppercase tracking-wide text-foreground/90"
+        className="mb-4 text-md font-extrabold uppercase tracking-wide text-foreground/90"
       >
         Connect
       </h2>
-      <address className="space-y-4 font-bold tracking-wide not-italic">
-        <Button asChild variant="glass" className="block rounded-xl w-fit">
-          <a
-            href="mailto:hi@keving.me"
-            aria-label="Send email to Kevin Gonzalez"
-          >
+      <address className="grid space-y-1 not-italic">
+        <Button asChild variant="link" className="px-0! w-fit">
+          <a href="mailto:hi@keving.me" aria-label="Send email to Kevin Gonzalez">
+            <Send />
             hi@keving.me
           </a>
         </Button>
-        <Button asChild variant="glass" className="block rounded-xl w-fit">
+        <Button asChild variant="link" className="px-0! w-fit">
           <a
             href="https://linkedin.com/in/typekev"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="Visit Kevin Gonzalez LinkedIn profile (opens in new tab)"
           >
+            <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path d="M22 3.47059V20.5294C22 20.9194 21.8451 21.2935 21.5693 21.5693C21.2935 21.8451 20.9194 22 20.5294 22H3.47059C3.08056 22 2.70651 21.8451 2.43073 21.5693C2.15494 21.2935 2 20.9194 2 20.5294V3.47059C2 3.08056 2.15494 2.70651 2.43073 2.43073C2.70651 2.15494 3.08056 2 3.47059 2H20.5294C20.9194 2 21.2935 2.15494 21.5693 2.43073C21.8451 2.70651 22 3.08056 22 3.47059ZM7.88235 9.64706H4.94118V19.0588H7.88235V9.64706ZM8.14706 6.41177C8.14861 6.18929 8.10632 5.96869 8.02261 5.76255C7.93891 5.55642 7.81542 5.36879 7.65919 5.21039C7.50297 5.05198 7.31708 4.92589 7.11213 4.83933C6.90718 4.75277 6.68718 4.70742 6.46471 4.70588H6.41177C5.95934 4.70588 5.52544 4.88561 5.20552 5.20552C4.88561 5.52544 4.70588 5.95934 4.70588 6.41177C4.70588 6.86419 4.88561 7.29809 5.20552 7.61801C5.52544 7.93792 5.95934 8.11765 6.41177 8.11765C6.63426 8.12312 6.85565 8.0847 7.06328 8.00458C7.27092 7.92447 7.46074 7.80422 7.62189 7.65072C7.78304 7.49722 7.91237 7.31346 8.00248 7.10996C8.09259 6.90646 8.14172 6.6872 8.14706 6.46471V6.41177ZM19.0588 13.3412C19.0588 10.5118 17.2588 9.41177 15.4706 9.41177C14.8851 9.38245 14.3021 9.50715 13.7799 9.77345C13.2576 10.0397 12.8143 10.4383 12.4941 10.9294H12.4118V9.64706H9.64706V19.0588H12.5882V14.0529C12.5457 13.5403 12.7072 13.0315 13.0376 12.6372C13.3681 12.2429 13.8407 11.9949 14.3529 11.9471H14.4647C15.4 11.9471 16.0941 12.5353 16.0941 14.0176V19.0588H19.0353L19.0588 13.3412Z" />
+            </svg>
             LinkedIn
           </a>
         </Button>
-        <Button asChild variant="glass" className="block rounded-xl w-fit">
+        <Button asChild variant="link" className="px-0! w-fit">
           <a
             href="https://github.com/typekev"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="Visit Kevin Gonzalez GitHub profile (opens in new tab)"
           >
+            <svg
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="-267 288.9 264.5 225.1"
+              xmlSpace="preserve"
+            >
+              <path d="M-21.8,354.4c-0.8-0.9-1.3-2.3-1.2-3.5c0.9-20.1-1.8-39.6-8.6-58.6c-0.9-2.4-2-3.3-4.5-2.6c-5.6,1.7-11.4,3-16.9,5 c-15.2,5.5-29.1,13.5-42.6,22.2c-1.4,0.9-3.5,1.5-5.2,1.2c-22.1-3.9-44.3-3.7-66.5-0.3c-2.2,0.3-5-0.2-6.8-1.3 c-15.7-10.2-31.7-20.2-50-25c-15.1-4-12-4.8-16.3,8.9c-5.1,16.4-7.1,33.4-6,50.6c0.1,1.2-0.8,2.7-1.6,3.8 c-6.6,7.9-11.7,16.6-14.8,26.4c-6,19-4.8,38.2-1,57.3c7.5,37.5,32.8,63.8,70.2,70.3c19.3,3.4,39.2,3.7,57.3,5.2 c20.2-1.5,38.9-1.6,57.1-4.5c31.8-5.1,55.8-22,67.8-52.7c4.2-10.7,6.5-22.4,7.9-33.8C-0.3,397.9-4.6,374.3-21.8,354.4z M-39.5,458.8c-3.3,15.6-12.4,26.3-27.6,31.8c-14.7,5.4-29.9,7.6-45.4,8.6c-7.5,0.5-15,0.1-22.5,0.1c-20.2,0.4-40.4-0.4-59.9-6.2 c-24.5-7.3-35.5-21.9-36.2-47.5c-0.3-9.1,0.7-17.9,5-26.1c8.5-16.5,23.2-22.3,40.6-22.6c9.5-0.1,19.1,0.7,28.7,1.6 c20.1,1.8,40-0.1,60-1.2c8.8-0.5,17.8-0.7,26.4,0.8c18.5,3.2,32.5,21.6,32.8,42.3C-37.6,446.5-38.3,452.8-39.5,458.8z" />
+              <path d="M-191.5,424.5c-5.5,5.7-7.4,12.9-7.7,19.3c0,9.6,2.8,17.3,7.7,22.3c6.8,6.9,16.1,6.7,22.6-0.3c9.8-10.6,9.7-30.5,0-41 C-175.4,417.7-184.9,417.6-191.5,424.5z" />
+              <path d="M-99.4,423.7c-11.2,10.3-11.2,33,0,43.2c6.5,5.9,15.3,5.6,21.4-0.7c5.6-5.8,7.6-13.1,7.8-21c-0.2-7.9-2.2-15.1-7.9-20.9 C-84.2,418-93,417.8-99.4,423.7z" />
+            </svg>
             GitHub
           </a>
         </Button>
